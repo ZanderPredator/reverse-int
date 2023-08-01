78edac6524a73ead8aefc727920f52d8e5563787
Brel Nikolay <january_1st@rambler.ru>
2023-08-01 18:48:31 +0300

    fix: complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse (n) {  
+    let num = n;
+    if(num < 0){
+        num = Math.abs(num);
+    }
+    let changeStr = String(num).split('').reverse().join('')
+    return +changeStr
 }
